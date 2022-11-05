5897956553fb4e05f16851998160dbe2237eea26
bestexz <69451712+6310682577@users.noreply.github.com>
2022-11-05 21:35:31 +0700

    change name of class

--- sukejuru/sekujuru_app/models.py
+++ sukejuru/sekujuru_app/models.py
@@ -2,31 +2,27 @@ from django.db import models
 from django.contrib.auth.models import User
 
 # ที่ทำ class platform, genre และ season เพราะจะได้เรียงตามพวกนี้ละก็ใส่ได้หลายอันได้
-class anime_platforms(models.Model):
+class AnimePlatforms(models.Model):
     name = models.CharField(max_length=99)
 
-class genre(models.Model):
-    type = models.CharField(max_length=99)
+class Genres(models.Model):
+    name = models.CharField(max_length=99)
 
-class season(models.Model):
+class Seasons(models.Model):
+    name = models.CharField(max_length=99)
+
+class Days(models.Model):
     name = models.CharField(max_length=99)
 
 class Anime(models.Model):
     anime_id = models.IntegerField(primary_key=True)
     anime_name = models.CharField(max_length=99)
     description = models.CharField(max_length=999)
-    platform = models.ManyToManyField(anime_platforms)
-    # ที่ใข้แยกทีละอันเพราะจะได้สามารถใส่ anime ที่ออกอากาศ 2 วันต่ออาทิตย์ได้
-    MONDAY = models.BooleanField()
-    TUESDAY = models.BooleanField()
-    WEDNESDAY = models.BooleanField()
-    THURSDAY = models.BooleanField()
-    FRIDAY = models.BooleanField()
-    SATURDAY = models.BooleanField()
-    SUNDAY = models.BooleanField()
+    platform = models.ManyToManyField(AnimePlatforms)
+    day = models.ManyToManyField(Days)
     time = models.TimeField()
-    anime_genre = models.ManyToManyField(genre)
-    anime_season = models.ManyToManyField(season)
+    genre = models.ManyToManyField(Genres)
+    season = models.ManyToManyField(Seasons)
     rating = models.IntegerField()
  
 class Users(models.Model):
@@ -37,5 +33,5 @@ class Favorite(models.Model):
     user = models.ForeignKey(Users, on_delete=models.CASCADE)
     anime = models.ForeignKey(Anime, on_delete=models.CASCADE)
 
-    class Mete:
+    class Meta:
         unique_together = [['user', 'anime']]

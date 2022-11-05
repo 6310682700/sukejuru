415ec00bd5409ebca613d88a3af1fc00f9585487
bestexz <69451712+6310682577@users.noreply.github.com>
2022-11-05 22:14:45 +0700

    create first test and add str to models

--- sukejuru/sekujuru_app/models.py
+++ sukejuru/sekujuru_app/models.py
@@ -5,14 +5,27 @@ from django.contrib.auth.models import User
 class AnimePlatforms(models.Model):
     name = models.CharField(max_length=99)
 
+    def __str__(self):
+        return f'{self.name}'
+
 class Genres(models.Model):
     name = models.CharField(max_length=99)
+
+    def __str__(self):
+        return f'{self.name}'
 
 class Seasons(models.Model):
     name = models.CharField(max_length=99)
 
+    def __str__(self):
+        return f'{self.name}'
+
 class Days(models.Model):
     name = models.CharField(max_length=99)
+    
+    def __str__(self):
+        return f'{self.name}'
+    
 
 class Anime(models.Model):
     anime_id = models.IntegerField(primary_key=True)
@@ -24,14 +37,24 @@ class Anime(models.Model):
     genre = models.ManyToManyField(Genres)
     season = models.ManyToManyField(Seasons)
     rating = models.IntegerField()
+
+    def __str__(self):
+        return f'{self.anime_id}: {self.anime_name} {self.description} {self.platform} {self.day} {self.time} {self.genre} {self.season} {self.rating}'
+    
  
 class Users(models.Model):
     fav_anime = models.ManyToManyField(Anime, blank=True, through='Favorite')
     d_user = models.ForeignKey(User, on_delete=models.CASCADE)
 
+    def __str__(self):
+        return f'{self.d_user}: {self.fav_anime}'
+
 class Favorite(models.Model):
     user = models.ForeignKey(Users, on_delete=models.CASCADE)
     anime = models.ForeignKey(Anime, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f'{self.user}: {self.anime}'
 
     class Meta:
         unique_together = [['user', 'anime']]

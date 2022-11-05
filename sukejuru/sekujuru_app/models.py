c19770cbf6d1690484428b51aa8159aa976e38a3
6310682700 <69451450+6310682700@users.noreply.github.com>
2022-11-05 15:01:16 +0700

    add models no migrations

--- sukejuru/sekujuru_app/models.py
+++ sukejuru/sekujuru_app/models.py
@@ -1,5 +1,41 @@
 from django.db import models
 from django.contrib.auth.models import User
 
-# Create your models here.
+# ที่ทำ class platform, genre และ season เพราะจะได้เรียงตามพวกนี้ละก็ใส่ได้หลายอันได้
+class anime_platform(models.Model):
+    name = models.CharField(max_lenght=99)
 
+class genre(models.Model):
+    type = models.CharField(max_lenght=99)
+
+class season(models.Model):
+    name = models.CharField(max_lenght=99)
+
+class Anime(models.Model):
+    anime_id = models.IntegerField(primary_key=True)
+    anime_name = models.CharField(max_lenght=99)
+    description = models.CharField(max_lenght=999)
+    platform = models.ManyToManyField(anime_platform)
+    # ที่ใข้แยกทีละอันเพราะจะได้สามารถใส่ anime ที่ออกอากาศ 2 วันต่ออาทิตย์ได้
+    MONDAY = models.BooleanField()
+    TUESDAY = models.BooleanField()
+    WEDNESDAY = models.BooleanField()
+    THURSDAY = models.BooleanField()
+    FRIDAY = models.BooleanField()
+    SATURDAY = models.BooleanField()
+    SUNDAY = models.BooleanField()
+    time = models.TimeField()
+    anime_genre = models.ManyToManyField(genre)
+    anime_season = models.ManyToManyField(season)
+    rating = models.IntegerField()
+ 
+class Users(models.Model):
+    fav_anime = models.ManyToManyField(Anime, blank=True, through='Favorite')
+    d_user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+class Favorite(models.Model):
+    user = models.ForeignKey(Users, on_delete=models.CASCADE)
+    anime = models.ForeignKey(Anime, on_delete=models.CASCADE)
+
+    class Mete:
+        unique_together = [['user', 'anime']]

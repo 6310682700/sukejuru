30431573d12445990e11a9583f4f4f875cb40db1
Bulakorn Junboonmee <6310682577@cn330>
2022-11-12 15:47:05 +0700

    Create user app

--- sukejuru/sekujuru_app/models.py
+++ sukejuru/sekujuru_app/models.py
@@ -1,13 +1,4 @@
 from django.db import models
-from django.contrib.auth.models import User
-from django.db.models.signals import post_save
-from django.dispatch import receiver
-
-# สร้าง WebUser object เมื่อมีการ create user
-@receiver(post_save, sender=User)
-def create_user_picks(sender, instance, created, **kwargs):
-    if created:
-        WebUser.objects.create(d_user=instance)
 
 # ที่ทำ class platform, genre และ season เพราะจะได้เรียงตามพวกนี้ละก็ใส่ได้หลายอันได้
 class AnimePlatform(models.Model):
@@ -48,20 +39,3 @@ class Anime(models.Model):
 
     def __str__(self):
         return f'{self.anime_id}: {self.anime_name} {self.description} {self.platform} {self.day} {self.time} {self.genre} {self.season} {self.rating}'
-    
-class WebUser(models.Model):
-    fav_anime = models.ManyToManyField(Anime, blank=True, through='Favorite')
-    d_user = models.ForeignKey(User, on_delete=models.CASCADE)
-
-    def __str__(self):
-        return f'{self.d_user}: {self.fav_anime}'
-
-class Favorite(models.Model):
-    user = models.ForeignKey(WebUser, on_delete=models.CASCADE)
-    anime = models.ForeignKey(Anime, on_delete=models.CASCADE)
-
-    def __str__(self):
-        return f'{self.user}: {self.anime}'
-
-    class Meta:
-        unique_together = [['user', 'anime']]

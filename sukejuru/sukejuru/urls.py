d01635b725cfacb9eb86a0392f91b075d357a6d7
6310682700 <69451450+6310682700@users.noreply.github.com>
2022-11-12 16:57:01 +0700

    finish event calender but still on going countdown timer

--- sukejuru/sukejuru/urls.py
+++ sukejuru/sukejuru/urls.py
@@ -25,4 +25,5 @@ urlpatterns = [
     path('login', views.login_view, name='login'),
     path('logout', views.logout_view, name='logout'),
     path('home', views.home_view, name='home'),
+    path('calender', views.calender, name='calender')
 ]

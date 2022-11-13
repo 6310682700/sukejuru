0b589b5e59ec12e0edd0aef93e8b79798665ed59
6310682692 <6310682692@student.tu.ac.th>
2022-11-13 21:32:08 +0700

    Test model/views

--- sukejuru/sekujuru_app/Test_models.py
+++ sukejuru/sekujuru_app/Test_models.py
@@ -1,13 +1,16 @@
-from django.test import TestCase, client
-from .models import AnimePlatform, Genre, Season, Day, Anime
-from django.urls import reverse 
+from django.contrib import auth
 from django.contrib.auth.models import User
-from user.models import WebUser, Favorite
+from django.test import TestCase, client
+from django.urls import reverse
+from user.models import Favorite, WebUser
+
+from .models import Anime, AnimePlatform, Day, Genre, Season
+
 
 class testModel(TestCase):
 
     def setUp(self):
-        User.objects.create(username = "non")
+        User.objects.create(username = "non", password = "ang")
         AnimePlatform.objects.create(name = "phone")
         AnimePlatform.objects.create(name = "netflix")
         Day.objects.create(name = "Monday")
@@ -22,22 +25,31 @@ class testModel(TestCase):
         Anime.objects.first().platform.add(AnimePlatform.objects.get(id=1))
         Anime.objects.first().platform.add(AnimePlatform.objects.get(id=2))
         WebUser.objects.create(d_user = User.objects.first())
-        WebUser.objects.first().fav_anime.set(Anime.objects.all()) 
+        WebUser.objects.first().fav_anime.set(Anime.objects.all())
     
-    def test_anime_platform(self):
+    def test_anime_platform(self):                                                          # Test which platform anime in
         animes = Anime.objects.first()
 
         self.assertEqual(animes.platform.get(name = 'phone').name, "phone")
 
-    def test_anime_day(self):
+    def test_anime_day(self):                                                                # Test what day anime premiere
         day = Anime.objects.first()
         self.assertEqual(day.day.get(name = 'Monday').name, 'Monday')
 
-    def test_anime_non_day(self):
+    def test_anime_non_day(self):                                                            # Test that anime is not in this day (in this case teusday)
         nonday = Anime.objects.first()
         nonday = list(dict(nonday.day.all().values_list()).values())
         self.assertFalse('Tuesday' in nonday)
 
-    def test_anime_seasons(self):
+    def test_anime_seasons(self):                                                            # Test anime seasons
         seasons = Anime.objects.first()
         self.assertEqual(seasons.season.get(pk = 1).name, 'Winter')
+
+    def test_anime_genre(self):                                                               # Test genre of a anime
+        genre = Anime.objects.first()
+        self.assertEqual(genre.genre.get(name = 'Fantasy').name, 'Fantasy')
+
+    def test_anime_rating(self):                                                               # Test rating score
+        rate = Anime.objects.first()
+        self.assertEqual(rate.rating, 5)
+
--- sukejuru/sekujuru_app/Test_view.py
+++ sukejuru/sekujuru_app/Test_view.py
@@ -8,10 +8,11 @@ from .models import AnimePlatform, Genre, Season, Day, Anime
 from django.urls import reverse 
 from user.models import WebUser, Favorite
 
+
 class testView(TestCase):
 
     def setUp(self):
-        User.objects.create(username = "non", password = "ang")
+        User.objects.create(username = "non", password = "angsuvapattanakul")
         AnimePlatform.objects.create(name = "phone")
         AnimePlatform.objects.create(name = "netflix")
         Day.objects.create(name = "Monday")
@@ -24,28 +25,45 @@ class testView(TestCase):
         Anime.objects.first().platform.add(AnimePlatform.objects.get(id=1))
         Anime.objects.first().platform.add(AnimePlatform.objects.get(id=2))
         WebUser.objects.create(d_user = User.objects.first())
-        WebUser.objects.first().fav_anime.set(Anime.objects.all())  
+        WebUser.objects.first().fav_anime.set(Anime.objects.all()) 
     
-
-    def test_login(self):
-        self.client = Client()
-        response = self.client.post(reverse('login'), {"username": "non","password" :"ang"})
-        self.assertEqual(response.status_code, 200)
-        
+    def test_login(self):                                                                           # Test page is availible
+        self.client = Client()                                
+        response = self.client.post(reverse('login'), {"username": "non","password" :"angsuvapattanakul"})
+        self.assertEqual(response.status_code, 200) 
 
     def test_logout(self):
         self.client = Client()
-        response = self.client.post(reverse('login'), {"username": "non", "password" :"ang"})
+        response = self.client.post(reverse('login'), {"username": "non", "password" :"angsuvapattanakul"})
         response = self.client.get(reverse('logout'))
         self.assertEqual(response.status_code, 200)
 
     def test_user_homepage(self):
         self.client = Client()
-        response = self.client.post(reverse('home'), {"username": "non","password" :"ang"})
+        response = self.client.post(reverse('home'), {"username": "non","password" :"angsuvapattanakul"})
         self.assertEqual(response.status_code, 200)
 
     def test_user_about(self):
         self.client = Client()
-        response = self.client.post(reverse('about'), {"username": "non","password" :"ang"})
+        response = self.client.post(reverse('about'), {"username": "non","password" :"angsuvapattanakul"})
         self.assertEqual(response.status_code, 200)
+
+    def test_user_search(self):
+        self.client = Client()
+        response = self.client.post(reverse('search'), {"username": "non","password" :"angsuvapattanakul"})
+        self.assertEqual(response.status_code, 200)
+
+    def test_user_regist(self):                                                                          # Test user can register
+        self.client = Client()
+        response = self.client.post(reverse('register'), {"username": "non","password" :"angsuvapattanakul"})
+        self.assertEqual(response.status_code, 200)            
+
+    def test_user_login(self):
+        user = User.objects.first() 
+        login = self.client.post(reverse('home'), {"username": "non","password" :"angsuvapattanakul"})   # Test user can login with valid account
+        self.assertTrue(login) 
+    
+    def test_user_login_fail(self):                                                                      # Test user login with non-valid account
+        login = self.client.login(username='blablabla', password='heck') 
+        self.assertFalse(login)
 

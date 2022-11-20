1ec309bd6f2129cdbdb2a009013d74f582973845
Bulakorn Junboonmee <6310682577@cn330>
2022-11-20 23:30:07 +0700

    search page

--- sukejuru/user/models.py
+++ sukejuru/user/models.py
@@ -12,8 +12,19 @@ def create_user_picks(sender, instance, created, **kwargs):
 
 # Create your models here.
 class WebUser(models.Model):
-    fav_anime = models.ManyToManyField(Anime, blank=True)
+    fav_anime = models.ManyToManyField(Anime, blank=True, through='Favorite')
     d_user = models.ForeignKey(User, on_delete=models.CASCADE)
 
     def __str__(self):
         return f'{self.d_user}: {self.fav_anime}'
+
+# don't know why but if delete it, it will crash
+class Favorite(models.Model):
+    user = models.ForeignKey(WebUser, on_delete=models.CASCADE)
+    anime = models.ForeignKey(Anime, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f'{self.user}: {self.anime}'
+
+    class Meta:
+        unique_together = [['user', 'anime']]
